e0f00b01da8b89680de3127f74fa3bc5a398cb36
odito <odkolas@gmail.com>
2019-04-10 12:03:35 +0300

    Updates

---
+++ precache-manifest.0131cd514be54ba128ffad55744d0e68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "49225faa5712f33c0f72",
+    "url": "/contactManager/static/js/runtime~main.63e620a2.js"
+  },
+  {
+    "revision": "478c76766705ab6e9dcf",
+    "url": "/contactManager/static/js/main.837035e8.chunk.js"
+  },
+  {
+    "revision": "5ab1321804f655dfc967",
+    "url": "/contactManager/static/js/2.055d0b7c.chunk.js"
+  },
+  {
+    "revision": "478c76766705ab6e9dcf",
+    "url": "/contactManager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "5ab1321804f655dfc967",
+    "url": "/contactManager/static/css/2.96a110cc.chunk.css"
+  },
+  {
+    "revision": "3d1596c2db260a512ccaded037c3b257",
+    "url": "/contactManager/index.html"
+  }
+];
